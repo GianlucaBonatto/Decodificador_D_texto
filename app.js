6a05028bf5a16d6cb8329d496b8e1e70b0091ef4
Gian <Gianluca.bonatto1@gmail.com>
2024-03-08 19:12:23 -0300

    c0 - criando um decodificador de texto

---
+++ app.js
@@ -0,0 +1,28 @@
+
+
+function bntCriptografar () {};
+
+function criptografar (string) {
+    const resultado = string
+
+    .replaceAll("e", "enter")
+    .replaceAll("i", "imes")
+    .replaceAll("a", "ai")
+    .replaceAll("o", "ober")
+    .replaceAll("u", "ufat");
+
+    return resultado;
+}
+
+function descriptografar (string) {
+    const resultado = string
+
+    .replaceAll("enter", "e")
+    .replaceAll("imes", "i")
+    .replaceAll("ai", "a")
+    .replaceAll("ober", "o")
+    .replaceAll("ufat", "u");
+
+    return resultado;
+
+}
